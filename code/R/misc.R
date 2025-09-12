103b1579f117e976371e8fb5e7094afad6f6de79
Liezel Tamon <yfg436@cluster4.hpc.in.bmrc.ox.ac.uk>
2025-09-12 14:51:35 +0100

    Add initial state of receptor convergence function

--- code/R/misc.R
+++ code/R/misc.R
@@ -26,3 +26,15 @@
 ) {
   return(length(x))
 }
+
+# Repertoire convergence (currently applicable for single-cell,
+# For bulk need to deal with read signal or normalised reads)
+# Needs subsampling because ratio is sensitive to raw number of unique sequences
+# **Need to adapt subsampling function for this**
+# The lower the value the more convergent
+# Repertoire being convergent actually requires more resources so a repertoire being like that means that it is doing it for a purpose
+repertoire_convergence <- function (x_nt, x_aa) {
+  n_unique_nt_seq <- length(unique(x_nt))
+  n_unique_aa_seq <- length(unique(x_aa))
+  n_unique_aa_seq / n_unique_nt_seq
+}
