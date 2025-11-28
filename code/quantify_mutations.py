9f2a9028eb9b0a0fb78f61b1ff0c85228cfab4b9
Liezel Tamon <yfg436@cluster4.hpc.in.bmrc.ox.ac.uk>
2025-11-28 02:01:44 +0000

    Update

--- code/quantify_mutations.py
+++ code/quantify_mutations.py
@@ -1,27 +1,23 @@
 # %% Quantify mutations in BCR chains
 # env: dandelion
-# srun -p long,short --output=log.out --error=log.err python quantify_mutations.py --library_type=ig &
+# sbatch -J quantify_mutations -p long,short --output=%x.log.out --error=%x.log.err --wrap='python quantify_mutations.py'
 
 import os
-import argparse
+os.chdir("../..")
 import dandelion as ddl
 import pandas as pd
 from functools import reduce, partial
 
-os.chdir("../..")
-
 # %% Parameters
 
-# Parse command-line arguments
-parser = argparse.ArgumentParser(description="Quantify mutations.")
-parser.add_argument("--library_type", type=str, required=True, help="ddl.pp.check_contigs(..., library_type).")
-args = parser.parse_args()
+contig_dir = "code/airr/reannotate/contigs_separate_directories"
+ignore_files = ["meta.csv", "reannotate_logs", "SLE_batch_2_pool_1", "SLE_batch_2_pool_2"]
+library_type_id = "ig_rearranged"
+ddl_prefix = "all"
 
-library_type = args.library_type
-#library_type = "ig"
-
-airr_path = os.path.join("results/airr/postprocess/filtered", library_type, "airr_rearrangement.tsv")
-out_dir = os.path.join("results", "airr", "quantify_mutations", library_type)
+out_dir = os.path.join(
+    "results", "airr", "quantify_mutations", library_type_id, ddl_prefix
+)
 os.makedirs(out_dir, exist_ok=True)
 
 # %% Function
@@ -35,8 +31,21 @@ merge_dfs_partial = partial(merge_dfs)
 
 # %% ----- MAIN -----
 
-# Load reannotated AIRR to adata
-vdj = ddl.read_airr(airr_path)
+# %% Concatenate all ig contigs in one dandelion object
+
+src_ids = list(set([id for id in os.listdir(os.path.join(contig_dir, library_type_id, ddl_prefix))]) - set(ignore_files))
+assert len(src_ids) == len(set(src_ids)), "Duplicate src ids found"
+
+ddl_list = []
+for src_id in src_ids:
+    print(f"Loading {src_id} ...")
+    vdj_ddl = ddl.Dandelion(os.path.join(contig_dir, library_type_id, ddl_prefix, src_id, src_id, "dandelion", "all_contig_dandelion.tsv"))
+    ddl_list.append(vdj_ddl)
+vdj = ddl.concat(ddl_list)
+
+assert vdj.metadata.index.is_unique, "Duplicate cell barcodes found in the concatenated dandelion object"
+
+# %% Quantify mutations
 
 mu_df_list = []
 for bool in [True, False]:
@@ -70,7 +79,12 @@ mu_df = reduce(merge_dfs_partial, mu_df_list)
 mu_df = mu_df.loc[:, ~mu_df.columns.str.endswith("_y")]
 mu_df.sort_index(axis=1, inplace=True)
 
-mu_df.to_hdf(
-    os.path.join(out_dir, "mutations.h5"),
-    key="x"
-)
+# %% Save
+
+mu_df.reset_index(names="index_unique", inplace=True)
+mu_df.to_csv(os.path.join(out_dir, "mutations.csv"), index=False)
+
+# mu_df.to_hdf(
+#     os.path.join(out_dir, "mutations.h5"),
+#     key="x"
+# )
