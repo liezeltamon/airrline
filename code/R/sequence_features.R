c93f34b2b05eeb022364790b421e239f939e50e3
Liezel Tamon <yfg436@cluster1.hpc.in.bmrc.ox.ac.uk>
2026-01-17 10:46:29 +0000

    Add notes but kmer_usage() migrated to be independent

--- code/R/sequence_features.R
+++ code/R/sequence_features.R
@@ -12,7 +12,7 @@ library(immunarch)
 
 # 1. k-mer usage (nt or aa)
 
-# ** Double check that this uses sliding window, if not use scRepertoire::percentKmer() **
+# immunarch::getKmers() counts k-mers using 1-nt sliding window as desired
 tmp <- lapply(names(immdata$data), FUN = function(x) {
   cbind(grouping_id = rep(x), immdata$data[[x]])
 })
@@ -21,16 +21,21 @@ df <- as.data.frame(do.call("rbind", tmp))
 kmer_usage <- function(
     df, motif_length = 3, sequence_col, sequence_type, grouping_values, ...
 ) {
+  # Needs both CDR3.nt and CDR3.aa columns because immunarch::getKmer() is checking internally
+  # Create placeholder columns
+  df["CDR3.nt"] <- "X"
+  df["CDR3.aa"] <- "X"
+
   new_colname <- paste0("CDR3.", sequence_type)
   if (!identical(sequence_col, new_colname)) {
     df[[new_colname]] <- df[[sequence_col]]
     df[[sequence_col]] <- NULL
   }
   df_list <- split(df, f = grouping_values)
-  # .coding = FALSE always because the filtering for productive (coding) sequences
-  # uses immunarch-specific columns
+  #.coding Logical. If TRUE (by default) then removes all non-coding sequences from input data first.
+  # Uses immunarch-specific columns
   kmer_counts_df <- immunarch::getKmers(
-    df_list, motif_length, sequence_type, .coding = FALSE#, ...
+    df_list, motif_length, sequence_type, #...
   ) %>% 
     column_to_rownames("Kmer") %>% 
     t()
